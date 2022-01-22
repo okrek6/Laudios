acc59592b8ff09960fa08bdc07c18856c1589247
okrek6 <okrek6@gmail.com>
2022-01-22 14:00:04 -0500

    First Commit

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+import sys
+
+def main(argv):
+    print("hello world")
+    
+##to run this at the moment, you need to write, python .\hello-world.py /tmp
+
+if __name__ == '__main__':
+    main(sys.argv[1])
